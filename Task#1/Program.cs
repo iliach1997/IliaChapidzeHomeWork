323833dda4a6ff1cc5a3b0db61ee7e132e948d79
iliach1997 <90194981+iliach1997@users.noreply.github.com>
2023-11-28 21:19:39 +0400

    added 3 tasks

---
+++ Task#1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Enter your Name:");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter your LastName:");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Enter your Gmail:");
+            string UserGmail = Console.ReadLine();
+            string FullName = Name + " " + LastName;
+
+            Console.WriteLine(FullName);
+            Console.WriteLine(UserGmail);
+
+            //Implicit 
+            int a = 5;
+            float b = a;
+            double c = b;
+            //Explicit
+            double d = 5.4;
+            float f = (float)d;
+        }
+    }
+}
---
+++ Task#3/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] MyArray = new int[5];
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+
+                Console.WriteLine("Enter a number :");
+                MyArray[i] = Convert.ToInt32(Console.ReadLine());
+
+
+
+            }
+            Console.WriteLine("------------MAXNUMBER--------------");
+            var MaxNum = 0;
+            for (int j = 0; j < MyArray.Length; j++)
+            {
+
+                if (MyArray[j] > MaxNum)
+                {
+                    MaxNum = MyArray[j];
+
+                }
+            }
+            Console.WriteLine($"MaxNum: {MaxNum}");
+            Console.WriteLine("--------------MINNAMBER--------------");
+            var MinNum = MyArray[0];
+            for (int g = 0; g < MyArray.Length; g++)
+            {
+
+                if (MyArray[g] < MinNum)
+                {
+                    MinNum = MyArray[g];
+
+                }
+
+            }
+            Console.WriteLine($"MinNum: {MinNum}");
+            Console.WriteLine("--------------ArithmetiPass--------------");
+            var ArithmetiPass = 0;
+            for (int g = 0; g < MyArray.Length; g++)
+            {
+                ArithmetiPass = ArithmetiPass + MyArray[g];
+
+
+
+            }
+            ArithmetiPass = ArithmetiPass / MyArray.Length;
+
+            Console.WriteLine($"ArithmetiPass: {ArithmetiPass}");
+
+
+            Console.WriteLine("--------------3--------------");
+            int[] Arrays = new int[5];
+
+            for (int g = 0; g < MyArray.Length; g++)
+            {
+                var result = MyArray[g] % 3;
+                if (result == 0)
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        Arrays[i] = MyArray[g];
+                        Console.WriteLine($"number: {Arrays[i]}");
+                    }
+                }
+
+            }
+
+            Console.WriteLine("--------------3--------------");
+        }
+    }
+}
