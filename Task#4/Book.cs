75561b9701ea540caf9c374f2eb193e7cd6c827e
iliach1997 <90194981+iliach1997@users.noreply.github.com>
2023-12-10 16:13:30 +0400

    Library

---
+++ Task#4/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Book
+    {
+       public string Title { get; set; }
+       public string Author { get; set; }
+       public int AvailableCopies { get; set; }
+       public Book(string title,string author, int availablecopies) { 
+            Title = title;
+            Author = author;
+            AvailableCopies = availablecopies;
+       }
+
+       public Book()
+       {
+       }
+    }
+}
